5e935ec5242c25295ee2a611afb97d4bc1b2f351
Renan Nunes <rafaelnc22@gmail.com>
2024-01-29 13:57:50 -0300

    postagem e delete

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import homeLogo from '../../assets/homelogo.jpg'
+import logo from '../../assets/logo.svg'
 import './Home.css';
 import ListaPostagens from '../../Componentes/componente/postagens/listaPostagens/ListaPostagens';
 import ModalPostagem from '../../Componentes/componente/postagens/modalPostagem/ModalPostagem';
@@ -22,7 +22,7 @@ function Home() {
             </div>
   
             <div className="flex justify-center ">
-              <img src={homeLogo} alt="" className='w-2/3' />
+              <img src={logo} alt="" className='w-2/3' />
       
             </div>
           </div>
